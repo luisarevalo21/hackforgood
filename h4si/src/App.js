1f5eacf50be40a5f97bd166f9ddfe2bf8f159881
budhraja.ankita <budhraja.ankita@gmail.com>
2024-11-10 13:20:23 -0800

    homepage

--- h4si/src/App.js
+++ h4si/src/App.js
@@ -5,12 +5,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import LegalAid from './components/legalAid';
+import HomePage from './components/home'
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <LegalAid />
+      {/* <LegalAid /> */}
+      <HomePage />
     </ThemeProvider>
   );
 }
